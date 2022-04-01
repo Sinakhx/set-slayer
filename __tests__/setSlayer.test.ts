ea384b29646b5542921e5186497377955d1b7267
Sina Khodabandehloo <khodabandehloo.sina@gmail.com>
2022-04-01 13:10:30 +0430

    test: isEmpty, contains, has

--- __tests__/setSlayer.test.ts
+++ __tests__/setSlayer.test.ts
@@ -1,15 +1,34 @@
 import SmartSet from '../lib/setSlayer';
 
 describe("A suite for the Set-Slayer's SmartSet API", () => {
-    describe('empty: returns true if the set contains no elements', () => {
-        it('sets with no elements are empty', () => {
+    describe('isEmpty: returns true if the set contains no elements', () => {
+        it('should be empty if there are no elements', () => {
             const actual = new SmartSet().isEmpty();
             expect(actual).toBeTruthy();
         });
 
-        it('sets with elements are not empty', () => {
+        it('should not be empty if there are some elements', () => {
             const actual = new SmartSet([1]).isEmpty();
             expect(actual).toBeFalsy();
+        });
+    });
+    describe('contains (aka has): sets can report if they contain an element', () => {
+        it('should contain nothing if set is empty', () => {
+            const set = new SmartSet();
+            expect(set.has(1)).toBeFalsy();
+            expect(set.contains(1)).toBeFalsy();
+        });
+
+        it('should recognize the existing elements in the set', () => {
+            const set = new SmartSet([1, 2, 3]);
+            expect(set.has(1)).toBeTruthy();
+            expect(set.contains(1)).toBeTruthy();
+        });
+
+        it('should not find any non-existing elements in the set', () => {
+            const set = new SmartSet([1, 2, 3]);
+            expect(set.has(4)).toBeFalsy();
+            expect(set.contains(4)).toBeFalsy();
         });
     });
 });
